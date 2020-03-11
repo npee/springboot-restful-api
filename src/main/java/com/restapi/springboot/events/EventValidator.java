9558dbd26d5b7973c4161b7cbaa7d2f3aac81932
MyeongchanJin <jmcchan@naver.com>
2020-03-12 02:41:46 +0900

    Event 검증용 코드

---
+++ src/main/java/com/restapi/springboot/events/EventValidator.java
@@ -0,0 +1,24 @@
+package com.restapi.springboot.events;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import java.time.LocalDateTime;
+
+@Component
+public class EventValidator {
+
+    public void validate(EventDTO eventDTO, Errors errors) {
+        if(eventDTO.getBasePrice() > eventDTO.getMaxPrice() && eventDTO.getMaxPrice() != 0) {
+            errors.rejectValue("basePrice", "wrongValue", "BasePrice is wrong");
+            errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is wrong");
+        }
+
+        LocalDateTime endEventDateTime = eventDTO.getEndEventDateTime();
+        if (endEventDateTime.isBefore(eventDTO.getBeginEventDateTime()) ||
+                endEventDateTime.isBefore(eventDTO.getCloseEnrollmentDateTime()) ||
+                endEventDateTime.isBefore(eventDTO.getBeginEnrollmentDateTime())) {
+            errors.rejectValue("endEventDateTime", "wrongValue", "Wrong Date Time");
+        }
+    }
+}
