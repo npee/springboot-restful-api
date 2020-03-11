85efd855c3a6c64ad7724b4111540e38a3a27f9c
MyeongchanJin <jmcchan@naver.com>
2020-03-12 01:54:42 +0900

    역직렬화시 매핑될 DTO 클래스

---
+++ src/main/java/com/restapi/springboot/events/EventDTO.java
@@ -0,0 +1,26 @@
+package com.restapi.springboot.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class EventDTO {
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime beginEventDateTime;
+    private LocalDateTime endEventDateTime;
+    private String location; // (optional)
+    private int basePrice; // (optional)
+    private int maxPrice;  // (optional)
+    private int limitOfEnrollment;
+
+}
