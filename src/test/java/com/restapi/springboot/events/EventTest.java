ce55247c4882923d1c9f360d5f6debbebeeb9948
MyeongchanJin <jmcchan@naver.com>
2020-03-12 23:27:28 +0900

    가격, 지역을 판단하는 로직 테스트

--- src/test/java/com/restapi/springboot/events/EventTest.java
+++ src/test/java/com/restapi/springboot/events/EventTest.java
@@ -1,9 +1,13 @@
 package com.restapi.springboot.events;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class EventTest {
     @Test
     public void builder() {
@@ -27,6 +31,47 @@ public class EventTest {
         // Then
         assertThat(event.getName()).isEqualTo(name);
         assertThat(event.getDescription()).isEqualTo(description);
+    }
+
+    @Test
+    @Parameters
+    public void testFree(int basePrice, int maxPrice, boolean isFree) {
+        Event event = Event.builder()
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
+                .build();
+
+        event.update();
+
+        assertThat(event.isFree()).isEqualTo(isFree);
+    }
+
+    private Object[] parametersForTestFree() {
+        return new Object[] {
+                new Object[] {0, 0, true},
+                new Object[] {100, 0, false},
+                new Object[] {0, 100, false},
+        };
+    }
+
+    @Test
+    @Parameters
+    public void testOffline(String loacation, boolean isOffline) {
+        Event event = Event.builder()
+                .location("Npee Corp.")
+                .build();
+
+        event.update();
+
+        assertThat(event.isOffline()).isTrue();
+    }
+
+    private Object[] parametersForTestOffline() {
+        return new Object[] {
+                new Object[] {"강남", true},
+                new Object[] {null, false},
+                new Object[] {"  ", false},
+        };
     }
 
 }
